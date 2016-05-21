163d4982fd1e339fd28205db44c35e800b349821
Tomas Zaoral <zaoralt@gmail.com>
2016-05-21 14:16:48 +0100

    adding disqus comments

---
+++ script/disqus.js
@@ -0,0 +1,8 @@
+(function() {
+var d = document, s = d.createElement('script');
+
+s.src = '//rational-way.disqus.com/embed.js';
+
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
